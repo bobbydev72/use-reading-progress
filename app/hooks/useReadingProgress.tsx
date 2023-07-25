6cc15bf277c8769f427bc10ac66b72a04485070c
Dmitry Bobryshev <dmitry.bobryshev@brackets.sk>
2023-07-26 00:27:19 +0200

    feat(global): created scrollbar

--- app/hooks/useReadingProgress.tsx
+++ app/hooks/useReadingProgress.tsx
@@ -1,7 +1,37 @@
-import { useState } from 'react';
+'use client';
+
+import { useEffect, useState } from 'react';
+// import { throttle } from 'lodash';
 
 export function useReadingProgress() {
-  const [completion, setCompletion] = useState(0);
+  const storedCompletion = localStorage.getItem('readingProgress');
+
+  const [completion, setCompletion] = useState(
+    storedCompletion ? parseFloat(storedCompletion) : 0
+  );
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const totalScroll = document.documentElement.scrollTop;
+
+      const windowHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+
+      const progress = (totalScroll / windowHeight) * 100;
+
+      setCompletion(progress);
+      localStorage.setItem('readingProgress', progress.toString());
+    };
+
+    // TODO: make smooth scroll animation
+    // const throttledHandleScroll = throttle(handleScroll, 100);
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return completion;
 }
