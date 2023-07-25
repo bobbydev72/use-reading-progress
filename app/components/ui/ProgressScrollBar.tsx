30f0faf7ed002c6b4b34687a83215c63f263f75a
Dmitry Bobryshev <dmitry.bobryshev@brackets.sk>
2023-07-26 00:39:03 +0200

    fix(global): remove console log

--- app/components/ui/ProgressScrollBar.tsx
+++ app/components/ui/ProgressScrollBar.tsx
@@ -6,8 +6,6 @@ import { useReadingProgress } from '@/app/hooks/useReadingProgress';
 export const ProgressScrollBar: React.FC = () => {
   const completion = useReadingProgress();
 
-  console.log(completion);
-
   return (
     <span
       className="fixed top-0 bg-indigo-600 w-full h-1.5 z-50 before:absolute before:w-full before:h-1.5 before:t-0 before:translate-x-full before:bg-gray-200 rounded-full"
